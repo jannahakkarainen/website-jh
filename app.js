a042bd14d32eb69bb600f83f107be194c028a73f
Janna <jannahakkarainen@hotmail.com>
2023-10-07 12:02:07 +0200

    new version 2023-10-07

--- app.js
+++ app.js
@@ -3,6 +3,39 @@ const { engine } = require('express-handlebars'); // loads handlebars for Expres
 const port = 2020 // defines the port
 const app = express() // creates the Express application
 
+
+// MODEL (DATA)
+const sqlite3 = require('sqlite3')
+const db = new sqlite3.Database('projects-jh.db')
+
+// creates table projects at startup 
+db.run("CREATE TABLE projects (pid INTEGER PRIMARY KEY, pname TEXT NOT NULL, pyear INTEGER NOT NULL, pdesc TEXT NOT NULL, ptype TEXT NOT NULL, pimgURL TEXT NOT NULL)", (error) => { 
+  if (error) {
+  // tests error: display error console.log("ERROR: ", error) 
+  } else {
+  // tests error: no error, the table has been created console.log("---> Table projects created!") 
+
+
+  const projects=[
+  { "id":"1", "name":"Cv with html", "type":" school project", "desc": "The purpose of this project was to try out html for the first time", "year": 2023, "dev":"Python and OpenCV (Computer vision) library", "url":"/img/img1.png" }, 
+  { "id":"2", "name":"Cv with html and css", "type":" school project", "desc": " The purpose of this project was to try out html in combination with css for the first time", "year": 2023, "url":"/img/img2.png" }, 
+  { "id":"3", "name":"Cv with multiple pages", "type":"school project", "desc": " The purpose of this project was to try out html and css and to create a webpage with multiple pages for the first time", "year": 2023, "url":"/img/img3.png" } 
+  ]
+
+
+  // inserts projects 
+  projects.forEach( (oneProject) => {
+  db.run("INSERT INTO projects (pid, pname, pyear, pdesc, ptype, pimgURL) VALUES (?, ?, ?, ?, ?, ?)", [oneProject.id, oneProject.name, 
+  oneProject.year, oneProject.desc, oneProject.type, oneProject.url], (error) => { if (error) { 
+  console.log("ERROR: ", error) } else { 
+  console.log("Line added into the projects table!") } 
+  }) }) 
+  }
+  }) 
+  
+
+
+
 // defines handlebars engine
 app.engine('handlebars', engine());
 // defines the view engine to be handlebars
@@ -13,14 +46,9 @@ app.set('views', './views');
 // define static directory "public" to access css/ and img/
 app.use(express.static('public'))
 
-// MODEL (DATA)
-const humans = [
-    {"id": "0", "name": "Jerome"}, 
-    {"id": "1", "name": "Mira"},
-    {"id": "2", "name": "Linus"}, 
-    {"id": "3", "name": "Susanne"}, 
-    {"id": "4", "name": "Jasmin"}, 
-]
+
+
+
 
 // CONTROLLER (THE BOSS)
 // defines route "/"
