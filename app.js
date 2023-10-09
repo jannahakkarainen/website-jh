502ba6721180b61f48e00745b5082803ee9eb6cc
Janna <jannahakkarainen@hotmail.com>
2023-10-09 16:37:30 +0200

    new version 2023-10-09

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express') // loads the express package
 const { engine } = require('express-handlebars'); // loads handlebars for Express
 const port = 2020 // defines the port
 const app = express() // creates the Express application
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const connectSqlite3 = require('connect-sqlite3')
+const cookieParser = require('cookie-parser')
 
 
 // MODEL (DATA)
@@ -35,6 +39,80 @@ db.run("CREATE TABLE projects (pid INTEGER PRIMARY KEY, pname TEXT NOT NULL, pye
   
 
 
+// creates skills projects at startup
+db.run("CREATE TABLE skills (sid INTEGER PRIMARY KEY, sname TEXT NOT NULL, sdesc TEXT NOT NULL, stype TEXT NOT NULL)", (error) => { if (error) { 
+  // tests error: display error
+  console.log("ERROR: ", error)
+} else {
+// tests error: no error, the table has been created 
+console.log("---> Table skills created!") 
+const skills=[
+{"id":"1", "name": "C++", "type": "Programming language", "desc": "Programming with C++."},
+{"id":"2", "name": "NoSQL", "type": "Programming language", "desc": "Working with databases in NoSQL."},
+{"id":"3", "name": "html", "type": "Markup language", "desc": "Creating web pages with html."},
+{"id":"4", "name": "css", "type": "Stylesheet language", "desc": "Designing web pages with css."},
+{"id":"5", "name": "Javascript", "type": "Programming language", "desc": "Programming with Javascript."}, {"id":"6", "name": "Adobe-Programs", "type": "Photo editing programs", "desc": "Using all adobe programs to edit photos"},
+] 
+  // inserts skills
+skills.forEach( (oneSkill) => {
+db.run("INSERT INTO skills (sid, sname, sdesc, stype) VALUES (?, ?, ?, ?)", [oneSkill.id, oneSkill.name, oneSkill.desc, 
+oneSkill.type], (error) => {
+      if (error) {
+        console.log("ERROR: ", error)
+      } else {
+console.log("Line added into the skills table!") } 
+}) }) 
+} }) 
+
+
+
+//---------
+// SESSION
+//---------
+
+// store sessions in the database
+const SQLiteStore = connectSqlite3(session)
+
+// define the session
+app.use(session({
+  store: new SQLiteStore({db: "session-db.db"}),
+  "saveUninitialized": false,
+  "resave": false,
+  "secret": "s3cr3t@s3nt3nc3@123"
+}));
+
+
+
+// ----------
+// POST Forms
+// ----------
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+//
+
+//check the login and password of a user
+app.post('/login', (request, response) => {
+  const un = request.body.un
+  const pw = request.body.pw
+
+  if (un=="janna" & pw=="6789") {
+    console.log("janna is logged in!")
+    request.session.isAdmin = true
+    request.session.isLoggedIn = true
+    request.session.name = "Janna"
+    response.redirect('/')
+  } else {
+    console.log('Bad user and/or bad password')
+    request.session.isAdmin = false
+    request.session.isLoggedIn = false
+    request.session.name = ""
+    response.redirect('/login')
+  }
+})
+
+
 
 // defines handlebars engine
 app.engine('handlebars', engine());
@@ -46,36 +124,93 @@ app.set('views', './views');
 // define static directory "public" to access css/ and img/
 app.use(express.static('public'))
 
-
-
-
-
 // CONTROLLER (THE BOSS)
 // defines route "/"
 app.get('/', function(request, response){
-  response.render('home.handlebars')
-})
+    console.log("SESSION: ", request.session)
+    const model={
+      isLoggedIn: request.session.isLoggedIn,
+      name: request.session.name,
+      isAdmin: request.session.isAdmin
+    }
+  response.render('home.handlebars', model)
+});
 
 app.get('/about', function(request, response){
-  response.render('about.handlebars')
-})
+  const model={
+    isLoggedIn: request.session.isLoggedIn,
+    name: request.session.name,
+    isAdmin: request.session.isAdmin
+  }
+  response.render('about.handlebars', model)
+});
+
+
 
 app.get('/projects', function(request, response){
-  response.render('projects.handlebars')
+  db.all("SELECT * FROM projects", function (error, theProject){
+    if (error) {
+      const model = {
+        hasDatabaseError: true,
+        theError: error,
+        projects: [],
+        isLoggedIn: request.session.isLoggedIn,
+        name: request.session.name,
+        isAdmin: request.session.isAdmin
+      }
+      
+      
+      //renders the page with the model
+      response.render("projects.handlebars", model)
+    }
+    else {
+      const model = {
+        hasDatabaseError: false,
+        theError: "",
+        projects: theProject,
+        isLoggedIn: request.session.isLoggedIn,
+        name: request.session.name,
+        isAdmin: request.session.isAdmin
+      }
+         //renders the page with the model
+         response.render("projects.handlebars", model)
+
+    }
+  })
 })
 
+//renders the login page
+app.get('/login', (request, response) => {
+  const model={
+    isLoggedIn: request.session.isLoggedIn,
+    name: request.session.name,
+    isAdmin: request.session.isAdmin
+  }
+  response.render('login.handlebars', model);
+});
+
 app.get('/skills', function(request, response){
-  response.render('skills.handlebars')
+  const model={
+    isLoggedIn: request.session.isLoggedIn,
+    name: request.session.name,
+    isAdmin: request.session.isAdmin
+  }
+  response.render('skills.handlebars', model)
 })
 
 app.get('/contact', function(request, response){
-  response.render('contact.handlebars')
+  const model={
+    isLoggedIn: request.session.isLoggedIn,
+    name: request.session.name,
+    isAdmin: request.session.isAdmin
+  }
+  response.render('contact.handlebars', model)
 })
 
 
 // defines the final default route 404 NOT FOUND
-app.use(function(req,res){
-  res.status(404).render('404.handlebars');
+app.use(function(request,response){
+  response.status(404).render('404.handlebars');
 });
 
 // runs the app and listens to the port
